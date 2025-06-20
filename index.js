b92f90831df669475668bfa3338279fbc29446bc
Psyko38 <110417117+Psyko38@users.noreply.github.com>
2025-06-20 16:29:54 +0200

    add code

--- index.js
+++ index.js
@@ -1,87 +1,175 @@
 const tabs = document.querySelectorAll("canvas");
 const PAVG = document.querySelectorAll("#AVG");
 const MSG = document.querySelectorAll("#MSG");
+const Emoji = document.querySelectorAll("#Emoji");
+const h3 = document.querySelectorAll("h3");
+const section = document.querySelectorAll("section > div");
+const sectionH2 = document.querySelectorAll("section > div > h2");
+const ColorPallet = [
+	"bg-yellow-200",
+	"bg-yellow-600",
+	"bg-lime-200",
+	"bg-lime-700",
+	"bg-orange-300",
+	"bg-orange-600",
+];
 
-Setup(
-	tabs[0],
-	[5, 10, 15, 12, 10, 13, 18],
-	"Kg",
-	PAVG[0],
-	MSG[0],
-	["Be patient, the day will arrive 😴", "Time to go 🚗"],
-	40
-);
-Setup(tabs[1], [28, 32, 30, 30, 29, 34, 30], "°C", PAVG[1], MSG[1], [
-	"The temperature and cold 🥶",
-	"Temperature and normal 👌",
-	"The temperature and hot 🥵",
-	28,
-]);
-Setup(
-	tabs[2],
-	[52.3, 52.3, 52.3, 52.3, 52.3, 52.3, 52.3],
-	"%",
-	PAVG[2],
-	MSG[2],
-	[
-		"Humidity and low 👇",
-		"The humidity is normal 👌",
-		"The humidity is high 📈",
-	],
-	52.5
-);
-Setup(
-	tabs[3],
-	[800000, 800000, 800000, 800000, 800000, 800000, 800000],
-	" passages",
-	PAVG[3],
-	MSG[3],
-	[
-		"The number of bee passages and low 📉",
-		"The number of bee passages is normal 👌",
-		"The number of bee passages and high📈",
-	],
-	8000000
-);
+run();
+
+async function run() {
+	let live = await fetchs("http://10.69.0.140:3000/live");
+	let hisoty = await fetchs("http://10.69.0.140:3000/history");
+
+	let len = hisoty.honey.length;
+
+	Setup(
+		0,
+		[
+			hisoty.honey[len - 7].weight,
+			hisoty.honey[len - 6].weight,
+			hisoty.honey[len - 5].weight,
+			hisoty.honey[len - 4].weight,
+			hisoty.honey[len - 3].weight,
+			hisoty.honey[len - 2].weight,
+			hisoty.honey[len - 1].weight,
+		],
+		"Kg",
+		["Be patient, the day will arrive 😴", "Time to go 🚗"],
+		10,
+		live.honey.weight,
+		[]
+	);
+
+	len = hisoty.temperature.length;
+
+	Setup(
+		1,
+		[
+			hisoty.temperature[len - 7].degree,
+			hisoty.temperature[len - 6].degree,
+			hisoty.temperature[len - 5].degree,
+			hisoty.temperature[len - 4].degree,
+			hisoty.temperature[len - 3].degree,
+			hisoty.temperature[len - 2].degree,
+			hisoty.temperature[len - 1].degree,
+		],
+		"°C",
+		[
+			"The temperature is cold",
+			"Temperature is normal",
+			"The temperature is hot",
+		],
+		35,
+		live.temperature.degree,
+		["🥶", "👌", "🥵"]
+	);
+
+	len = hisoty.humidity.length;
+
+	Setup(
+		2,
+		[
+			hisoty.humidity[len - 7].percentage,
+			hisoty.humidity[len - 6].percentage,
+			hisoty.humidity[len - 5].percentage,
+			hisoty.humidity[len - 4].percentage,
+			hisoty.humidity[len - 3].percentage,
+			hisoty.humidity[len - 2].percentage,
+			hisoty.humidity[len - 1].percentage,
+		],
+		"%",
+		["Humidity is low", "The humidity is normal", "The humidity is high"],
+		18.5,
+		live.humidity.percentage,
+		["👇", "👌", "📈"]
+	);
+
+	len = hisoty.flow.length;
+
+	Setup(
+		3,
+		[
+			hisoty.flow[len - 7].count,
+			hisoty.flow[len - 6].count,
+			hisoty.flow[len - 5].count,
+			hisoty.flow[len - 4].count,
+			hisoty.flow[len - 3].count,
+			hisoty.flow[len - 2].count,
+			hisoty.flow[len - 1].count,
+		],
+		" passages",
+		[
+			"The number of bee passages is low",
+			"The number of bee passages is normal",
+			"The number of bee passages is high",
+		],
+		0,
+		live.flow.count,
+		["📉", "👌", "📈"]
+	);
+}
+
+async function fetchs(api) {
+	let data = await fetch(api);
+	return await data.json();
+}
 
 function Setup(
-	element,
+	idx,
 	datas,
 	name,
-	AVGtxt,
-	MSGtxt,
 	MSGPosibleMessage,
-	baseValue
+	baseValues,
+	h3value,
+	Emojis
 ) {
 	let avg = 0;
 	for (let i of datas) {
 		avg = avg + i;
 	}
 	avg = avg / datas.length;
+
+	let baseValue = baseValues;
+	if (baseValue == 0) baseValue = avg;
 	if (MSGPosibleMessage.length == 2) {
 		let min = baseValue - baseValue * 0.2;
 		if (min >= avg) {
-			MSGtxt.innerText = MSGPosibleMessage[0];
+			MSG[idx].innerText = MSGPosibleMessage[0];
+			section[idx].classList.add(ColorPallet[0]);
+			sectionH2[idx].classList.add(ColorPallet[1]);
 		} else {
-			MSGtxt.innerText = MSGPosibleMessage[1];
+			MSG[idx].innerText = MSGPosibleMessage[1];
+			section[idx].classList.add(ColorPallet[2]);
+			sectionH2[idx].classList.add(ColorPallet[3]);
 		}
 	} else {
 		let min = baseValue - baseValue * 0.2;
 		let max = baseValue + baseValue * 0.2;
 
 		if (avg >= max) {
-			MSGtxt.innerText = MSGPosibleMessage[2];
+			MSG[idx].innerText = MSGPosibleMessage[2];
+			Emoji[idx - 1].innerText = Emojis[2];
+			section[idx].classList.add(ColorPallet[4]);
+			sectionH2[idx].classList.add(ColorPallet[5]);
 		} else {
 			if (avg <= min) {
-				MSGtxt.innerText = MSGPosibleMessage[0];
+				MSG[idx].innerText = MSGPosibleMessage[0];
+				Emoji[idx - 1].innerText = Emojis[0];
+				section[idx].classList.add(ColorPallet[4]);
+				sectionH2[idx].classList.add(ColorPallet[5]);
 			} else {
-				MSGtxt.innerText = MSGPosibleMessage[1];
+				MSG[idx].innerText = MSGPosibleMessage[1];
+				Emoji[idx - 1].innerText = Emojis[1];
+				section[idx].classList.add(ColorPallet[2]);
+				sectionH2[idx].classList.add(ColorPallet[3]);
 			}
 		}
 	}
 
-	AVGtxt.innerText = `AVG : ${Math.floor(avg)}${name}`;
-	new Chart(element, {
+	h3[idx].textContent = `${h3value}${name}`;
+
+	PAVG[idx].innerText = `AVG : ${avg.toFixed(2)}${name}`;
+	new Chart(tabs[idx], {
 		type: "bar",
 		data: {
 			labels: ["J-7", "J-6", "J-5", "J-4", "J-3", "J-2", "J-1"],
